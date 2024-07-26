e77e55e3f1db4235285d7d13a03fa91782849841
sabinks <sabin.kr.stha@gmail.com>
2024-07-26 14:43:03 +0545

    payment integration

--- src/stripe/stripe.controller.ts
+++ src/stripe/stripe.controller.ts
@@ -1,12 +1,37 @@
-import { Controller, Get, } from '@nestjs/common';
+import { Controller, Get, Query, } from '@nestjs/common';
 import { StripeService } from './stripe.service';
 
-@Controller('/api/next/get-stripe-intent')
+@Controller('/api/')
 export class StripeController {
-    constructor(private readonly stripeService: StripeService,
+    constructor(
+        private readonly stripeService: StripeService,
     ) { }
-    @Get('/')
-    checkAppointmentAvailability() {
-        return this.stripeService.createStripeIntent()
+
+    @Get('create-stripe-intent')
+    createStripeIntent(
+        @Query('amt') amt: string,
+        @Query('cur') cur: string,
+        @Query('cusId') cusId: string,
+        @Query('refId') refId: number,
+    ) {
+
+        return this.stripeService.createStripeIntent(amt, cur, cusId, +refId)
+    }
+
+    @Get('payment-success')
+    paymentSuccess(
+        @Query('paymentId') paymentId: string,
+        @Query('cusId') cusId: string,
+        @Query('refId') refId: number,
+    ) {
+
+        return this.stripeService.paymentSuccess(paymentId, cusId, +refId)
+    }
+
+    @Get('cancel-stripe-intent')
+    cancelStripeIntent(
+        @Query('paymentIntent') paymentIntent: string,
+    ) {
+        return this.stripeService.cancelStripeIntent(paymentIntent)
     }
 }
--- src/stripe/stripe.service.ts
+++ src/stripe/stripe.service.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { ForbiddenException, Inject, Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import Stripe from 'stripe';
 
@@ -8,19 +8,83 @@ export class StripeService {
         private prisma: PrismaService,
         @Inject('STRIPE_CLIENT') private stripe: Stripe
     ) { }
-
-    async createStripeIntent() {
+    async checkPaymentExists(cusId, refId) {
+        let bookAppointment = this.prisma.bookAppointment.findFirst({
+            where: {
+                id: refId
+            }
+        })
+        if ((await bookAppointment).paymentId) {
+            return {
+                message: 'Payment Completed',
+            }
+        }
+    }
+    async createStripeIntent(amt, cur, cusId, refId) {
+        let bookAppointment = this.prisma.bookAppointment.findFirst({
+            where: {
+                id: refId
+            }
+        })
+        if ((await bookAppointment).paymentId) {
+            throw new ForbiddenException('Payment successful, no action required!')
+        }
+        let user = this.prisma.user.findFirst({
+            where: {
+                customerId: cusId
+            }
+        })
+        if (!user) {
+            throw new NotFoundException('User not found')
+        }
         let paymentIntent = await this.stripe.paymentIntents.create({
             currency: process.env.STRIPE_CURRENCY,
-            amount: 1000,
-            customer: 'id',
+            amount: amt * 100,
+            customer: cusId,
             metadata: {
-                name: 'username',
-                email: 'email@mail.com',
-                service: 'Book Appointment',
+                name: (await user).username,
+                email: (await user).email,
+                service: 'Book Appointment Payment',
                 status: 'Completed'
             }
         })
         return paymentIntent
+    }
+
+
+    async paymentSuccess(paymentId, cusId, refId) {
+        let user = await this.prisma.user.findFirst({
+            where: {
+                customerId: cusId
+            }
+        })
+        if (!user) {
+            throw new NotFoundException('User not found')
+        }
+        let bookAppointment = await this.prisma.bookAppointment.findFirst({
+            where: {
+                id: refId
+            }
+        })
+        if (!bookAppointment) {
+            throw new NotFoundException('Appointment not found')
+        }
+        await this.prisma.bookAppointment.update({
+            where: {
+                id: refId
+            },
+            data: {
+                paymentStatus: 'Payment Completed',
+                paymentId: paymentId
+            }
+        })
+
+        return "Payment succesful"
+    }
+
+    async cancelStripeIntent(paymentIntent: string) {
+
+        let status = await this.stripe.paymentIntents.cancel(paymentIntent)
+        return status
     }
 }
